d0def513ec30bcfdae4c277e949d7be81bdc5e4c
khantuevao <khantuevao@gmail.com>
2022-07-25 17:39:41 +0200

    Adjust dropdown menu animation

--- src/index.js
+++ src/index.js
@@ -2,12 +2,17 @@ console.log("hello")
 
 const layer = document.querySelector('.layer');
 const dropdown = document.querySelector('.dropdown');
+const links = document.getElementsByClassName('link');
 
 const menuOpen = document.getElementById('menu-open');
 menuOpen.addEventListener('click', () => {
   menuOpen.classList.add('hidden');
   layer.classList.add('visible');
   dropdown.classList.add('visible');
+  menuClose.classList.add('visible')
+  for (let i = 0; i < links.length; i++) {
+    links[i].classList.add('visible');
+  }
 })
 
 const menuClose = document.getElementById('menu-close');
@@ -15,4 +20,8 @@ menuClose.addEventListener('click', () => {
   layer.classList.remove('visible');
   dropdown.classList.remove('visible');
   menuOpen.classList.remove('hidden');
+  menuClose.classList.remove('visible')
+  for (let i = 0; i < links.length; i++) {
+    links[i].classList.remove('visible');
+  }  
 })
