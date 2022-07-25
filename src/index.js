8436bd516abe04454a976b5763577ccac4b6bbe0
khantuevao <khantuevao@gmail.com>
2022-07-25 16:48:26 +0200

    Animate dropdown menu

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 console.log("hello")
+
+const layer = document.querySelector('.layer');
+const dropdown = document.querySelector('.dropdown');
+
+const menuOpen = document.getElementById('menu-open');
+menuOpen.addEventListener('click', () => {
+  menuOpen.classList.add('hidden');
+  layer.classList.add('visible');
+  dropdown.classList.add('visible');
+})
+
+const menuClose = document.getElementById('menu-close');
+menuClose.addEventListener('click', () => {
+  layer.classList.remove('visible');
+  dropdown.classList.remove('visible');
+  menuOpen.classList.remove('hidden');
+})
