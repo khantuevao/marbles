a44085cef22a8a81b649d4aba02307408b5299c7
khantuevao <khantuevao@gmail.com>
2022-07-25 20:03:57 +0200

    Minor changes to css

--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
-console.log("hello")
+console.log("hello");
 
-const layer = document.querySelector('.layer');
-const dropdown = document.querySelector('.dropdown');
-const links = document.getElementsByClassName('link');
+const layer = document.querySelector(".layer");
+const dropdown = document.querySelector(".dropdown");
+const links = document.getElementsByClassName("link");
 
-const menuOpen = document.getElementById('menu-open');
-menuOpen.addEventListener('click', () => {
-  layer.classList.add('visible');
-  dropdown.classList.add('visible');
-  menuClose.classList.add('visible')
+const menuOpen = document.getElementById("menu-open");
+menuOpen.addEventListener("click", () => {
+  layer.classList.add("visible");
+  dropdown.classList.add("visible");
+  menuClose.classList.add("visible");
   for (let i = 0; i < links.length; i++) {
-    links[i].classList.add('visible');
+    links[i].classList.add("visible");
   }
-})
+});
 
-const menuClose = document.getElementById('menu-close');
-menuClose.addEventListener('click', () => {
-  layer.classList.remove('visible');
-  dropdown.classList.remove('visible');
-  menuOpen.classList.remove('hidden');
-  menuClose.classList.remove('visible')
+const menuClose = document.getElementById("menu-close");
+menuClose.addEventListener("click", () => {
+  layer.classList.remove("visible");
+  dropdown.classList.remove("visible");
+  menuOpen.classList.remove("hidden");
+  menuClose.classList.remove("visible");
   for (let i = 0; i < links.length; i++) {
-    links[i].classList.remove('visible');
-  }  
-})
+    links[i].classList.remove("visible");
+  }
+});
